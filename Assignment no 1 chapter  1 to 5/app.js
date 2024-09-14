8b56e8f84e2aa21f78ef4068e90cd81e2c504b55
HashamSalauddin2006 <hashamsalauddin2006@gmail.com>
2024-09-14 10:22:42 +0500

    chapter no 2 complete

--- Assignment no 1 chapter  1 to 5/app.js
+++ Assignment no 1 chapter  1 to 5/app.js
@@ -2,24 +2,97 @@
 
 // ************* Question no 1************* 
 
-alert ("greet your website visitor");
+// alert ("greet your website visitor");
 
 // ************* Question no 2 ************* 
 
-alert ("Error ! Please enter a valid password");
+// alert ("Error ! Please enter a valid password");
 
 // ************* Question no 3 ************* 
 
- alert ("Welcome to JS  land  Happy Coding");
+//  alert ("Welcome to JS  land  Happy Coding");
 
 // ************* Question no 4 *************
 
-alert ("Welcome to Js Land");
-alert ("Happy Coding");
+// alert ("Welcome to Js Land");
+// alert ("Happy Coding");
 
 // ************* Question no 5 *************
 
-console.log ("Hello... I can run JS throught my web browser's console");    
+// console.log ("Hello... I can run JS throught my web browser's console");    
+
+// chapter no 1 Complete
+
+
+// ============ chapter no 2  ============== 
+
+// ************* Question no 1 ************* 
+
+var usernme;
+
+// ************* Question no 2 ************* 
+
+var myName = "Hammas";
+
+// ************* Question no 3 ************* 
+
+var message = "Hello World";
+alert(message);
+
+// ************* Question no 4 ************* 
+
+var studentName  = "hammas";
+
+alert ( "student Name" + " " +  studentName);
+
+var studentAge  = 20;
+
+alert ( "student Age" + " " +  studentAge);
+
+var studentCourse  = "web and app development";
+
+alert ( "student Course" + " " +  studentCourse);
+
+// *************** Question no 5 ***************
+
+
+var string = "pizza pizza";
+alert(string);
+
+// ************* Question no 6  ************* 
+
+var email = "hammas2580@gmail.com";
+alert ("My email address is" + " " + email);
+
+// ************* Question no 7  ************* 
+
+var book = "A Smater Way to Learn JavaScript";
+alert (book);
+
+
+// ************* Question no 8  ************* 
+
+document.write ("Yah ! I can write HTML content trought Java script");
+
+// ************* Question no 9  ************* 
+
+var value = "hammas" ;
+alert (value);
+console.log(value);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
