1335879f6890460266dc59186323b0d0f9220ed2
HashamSalauddin2006 <hashamsalauddin2006@gmail.com>
2024-09-14 09:05:00 +0500

    chapter no 1 done (alert)

---
+++ Assignment no 1 chapter  1 to 5/app.js
@@ -0,0 +1,27 @@
+// ============ chapter no 1 ============== 
+
+// ************* Question no 1************* 
+
+alert ("greet your website visitor");
+
+// ************* Question no 2 ************* 
+
+alert ("Error ! Please enter a valid password");
+
+// ************* Question no 3 ************* 
+
+ alert ("Welcome to JS  land  Happy Coding");
+
+// ************* Question no 4 *************
+
+alert ("Welcome to Js Land");
+alert ("Happy Coding");
+
+// ************* Question no 5 *************
+
+console.log ("Hello... I can run JS throught my web browser's console");    
+
+
+
+
+
